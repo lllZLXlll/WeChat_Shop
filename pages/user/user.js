23622e4c15d5a1b09fdd48dc3e828f1a5544a66a
ZLXChengXuYuan <846117898@qq.com>
2017-12-09 21:19:13 +0800

    我的
    样式微调

--- pages/user/user.js
+++ pages/user/user.js
@@ -9,22 +9,86 @@ Page({
       avatarUrl: null,
     },
   },
-  
-  // 用户登录
-  userLogin: function() {
-    var _this = this;  
-    wx.login({
-      success: function(e) {
-        // 登录成功后获取用户信息
-        wx.getUserInfo({
-          success: function(e) {
-            _this.setData({
-              userInfo: e.userInfo
-            });
-          }
-        })
+
+  onLoad: function(e) {
+    var _this = this;
+    // 查看是否获取了用户信息
+    wx.getSetting({
+      success(res) {
+        // 没有获取，就获取用户信息
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success(e) { // 用户同意直接获取
+              wx.getUserInfo({
+                success(e) {
+                  _this.setData({
+                    userInfo: e.userInfo
+                  });
+                }
+              })
+            },
+            fail(e) { // 用户拒绝直接跳转开启授权页面，引导用户开启
+              wx.openSetting({
+                success(e) {
+                  // 用户同意授权
+                  if (e.authSetting['scope.userInfo'] == true) {
+                    wx.getUserInfo({
+                      success(e) {
+                        _this.setData({
+                          userInfo: e.userInfo
+                        });
+                      }
+                    })
+                  }
+                },
+              })
+            }
+          })
+        }
       }
     })
+  },
+  
+  // 获取用户信息
+  getUserInfo: function() {
+    var _this = this;  
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userInfo']) {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success(e) {
+              wx.getUserInfo({
+                success(e) {
+                  _this.setData({
+                    userInfo: e.userInfo
+                  });
+                }
+              })
+            },
+          })
+        } else {
+          if (_this.data.userInfo.nickName == null) {
+            wx.openSetting({
+              success(e) {
+                // 用户同意授权
+                if (e.authSetting['scope.userInfo'] == true) {
+                  wx.getUserInfo({
+                    success(e) {
+                      _this.setData({
+                        userInfo: e.userInfo
+                      });
+                    }
+                  })
+                }
+              },
+            })
+          }
+        }
+      }
+    })
+
   }
 
 })
