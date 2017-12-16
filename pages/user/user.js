c029a31187a4c5053843ac82d232d21fbd800a8d
ZLXChengXuYuan <846117898@qq.com>
2017-12-16 16:52:44 +0800

    我的
    用户登录

--- pages/user/user.js
+++ pages/user/user.js
@@ -10,57 +10,79 @@ Page({
     },
   },
 
-  onLoad: function(e) {
-    var _this = this;
-    // 查看是否获取了用户信息
-    wx.getSetting({
-      success(res) {
-        // 没有获取，就获取用户信息
-        if (!res.authSetting['scope.userInfo']) {
-          wx.authorize({
-            scope: 'scope.userInfo',
-            success(e) { // 用户同意直接获取
-              wx.getUserInfo({
-                success(e) {
-                  _this.setData({
-                    userInfo: e.userInfo
-                  });
-                }
-              })
+  onLoad: function (e) {
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          console.log(res)
+
+          //发起网络请求
+          wx.request({
+            url: 'http://127.0.0.1:8080/zlx/login', //仅为示例，并非真实的接口地址
+            data: {
+              jdCode: res.code,
             },
-            fail(e) { // 用户拒绝直接跳转开启授权页面，引导用户开启
-              wx.openSetting({
-                success(e) {
-                  // 用户同意授权
-                  if (e.authSetting['scope.userInfo'] == true) {
-                    wx.getUserInfo({
-                      success(e) {
-                        _this.setData({
-                          userInfo: e.userInfo
-                        });
-                      }
-                    })
-                  }
-                },
-              })
-            }
-          })
-        } else {
-          wx.getUserInfo({
-            success(e) {
-              _this.setData({
-                userInfo: e.userInfo
-              });
+            header: {
+              'content-type': 'application/json' // 默认值
+            },
+            success: function (res) {
+              console.log(res.data)
             }
           })
         }
       }
-    })
+    });
+
+    // var _this = this;
+    // // 查看是否获取了用户信息
+    // wx.getSetting({
+    //   success(res) {
+    //     // 没有获取，就获取用户信息
+    //     if (!res.authSetting['scope.userInfo']) {
+    //       wx.authorize({
+    //         scope: 'scope.userInfo',
+    //         success(e) { // 用户同意直接获取
+    //           wx.getUserInfo({
+    //             success(e) {
+    //               _this.setData({
+    //                 userInfo: e.userInfo
+    //               });
+    //             }
+    //           })
+    //         },
+    //         fail(e) { // 用户拒绝直接跳转开启授权页面，引导用户开启
+    //           wx.openSetting({
+    //             success(e) {
+    //               // 用户同意授权
+    //               if (e.authSetting['scope.userInfo'] == true) {
+    //                 wx.getUserInfo({
+    //                   success(e) {
+    //                     _this.setData({
+    //                       userInfo: e.userInfo
+    //                     });
+    //                   }
+    //                 })
+    //               }
+    //             },
+    //           })
+    //         }
+    //       })
+    //     } else {
+    //       wx.getUserInfo({
+    //         success(e) {
+    //           _this.setData({
+    //             userInfo: e.userInfo
+    //           });
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
   },
-  
+
   // 获取用户信息
-  getUserInfo: function() {
-    var _this = this;  
+  getUserInfo: function () {
+    var _this = this;
     wx.getSetting({
       success(res) {
         if (!res.authSetting['scope.userInfo']) {
