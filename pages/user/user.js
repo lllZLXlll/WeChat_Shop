632c53510565e557fcb289dd67420a5ff7f7b546
ZLXChengXuYuan <846117898@qq.com>
2017-12-17 12:55:02 +0800

    我的
    用户登录信息保存，异常后重新获取数据在保存

--- pages/user/user.js
+++ pages/user/user.js
@@ -11,117 +11,300 @@ Page({
   },
 
   onLoad: function (e) {
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          console.log(res)
-
-          //发起网络请求
-          wx.request({
-            url: 'http://127.0.0.1:8080/zlx/login', //仅为示例，并非真实的接口地址
-            data: {
-              jdCode: res.code,
-            },
-            header: {
-              'content-type': 'application/json' // 默认值
-            },
+    var _this = this;
+    // 查看是否获取了用户信息
+    wx.getSetting({
+      success(res) {
+        // 没有获取，就获取用户信息
+        if (!res.authSetting['scope.userInfo']) {
+          wx.login({
             success: function (res) {
-              console.log(res.data)
+              if (res.code) {
+                wx.getUserInfo({
+                  lang: 'zh_CN',
+                  success(e) {
+                    console.log(e)
+                    _this.setData({
+                      userInfo: e.userInfo
+                    });
+                    //发起网络请求
+                    wx.request({
+                      url: 'http://127.0.0.1:8080/zlx/login',
+                      data: {
+                        jdCode: res.code,
+                        nickName: e.userInfo.nickName,
+                        gender: e.userInfo.gender,
+                        country: e.userInfo.country,
+                        province: e.userInfo.province,
+                        city: e.userInfo.city,
+                        avatarUrl: e.userInfo.avatarUrl,
+                      },
+                      header: {
+                        'content-type': 'application/json' // 默认值
+                      },
+                      success: function (res) {
+                        console.log(res.data)
+                        if (res.data.error == 'code-0000') {
+                          // 登录成功，用户信息成功保存
+                        } else {
+                          // 用户信息保存失败,重新获取
+                          wx.login({
+                            success: function (res) {
+                              if (res.code) {
+                                wx.getUserInfo({
+                                  lang: 'zh_CN',
+                                  success(e) {
+                                    console.log(e)
+                                    _this.setData({
+                                      userInfo: e.userInfo
+                                    });
+                                    //发起网络请求
+                                    wx.request({
+                                      url: 'http://127.0.0.1:8080/zlx/login',
+                                      data: {
+                                        jdCode: res.code,
+                                        nickName: e.userInfo.nickName,
+                                        gender: e.userInfo.gender,
+                                        country: e.userInfo.country,
+                                        province: e.userInfo.province,
+                                        city: e.userInfo.city,
+                                        avatarUrl: e.userInfo.avatarUrl,
+                                      },
+                                      header: {
+                                        'content-type': 'application/json' // 默认值
+                                      },
+                                      success: function (res) {
+                                        console.log(res.data)
+                                        if (res.data.error == 'code-0000') {
+                                          // 登录成功，用户信息成功保存
+                                        }
+                                      }
+                                    })
+                                  }
+                                });
+                              }
+                            }
+                          });
+                        }
+                      }
+                    })
+                  },
+                  fail(e) {
+                    wx.openSetting({
+                      success(e) {
+                        // 用户同意授权
+                        if (e.authSetting['scope.userInfo'] == true) {
+                          wx.login({
+                            success: function (res) {
+                              if (res.code) {
+                                wx.getUserInfo({
+                                  lang: 'zh_CN',
+                                  success(e) {
+                                    console.log(e)
+                                    _this.setData({
+                                      userInfo: e.userInfo
+                                    });
+                                    //发起网络请求
+                                    wx.request({
+                                      url: 'http://127.0.0.1:8080/zlx/login',
+                                      data: {
+                                        jdCode: res.code,
+                                        nickName: e.userInfo.nickName,
+                                        gender: e.userInfo.gender,
+                                        country: e.userInfo.country,
+                                        province: e.userInfo.province,
+                                        city: e.userInfo.city,
+                                        avatarUrl: e.userInfo.avatarUrl,
+                                      },
+                                      header: {
+                                        'content-type': 'application/json' // 默认值
+                                      },
+                                      success: function (res) {
+                                        console.log(res.data)
+                                        if (res.data.error == 'code-0000') {
+                                          // 登录成功，用户信息成功保存
+                                        } else {
+                                          // 用户信息保存失败,重新获取
+                                          wx.login({
+                                            success: function (res) {
+                                              if (res.code) {
+                                                wx.getUserInfo({
+                                                  lang: 'zh_CN',
+                                                  success(e) {
+                                                    console.log(e)
+                                                    _this.setData({
+                                                      userInfo: e.userInfo
+                                                    });
+                                                    //发起网络请求
+                                                    wx.request({
+                                                      url: 'http://127.0.0.1:8080/zlx/login',
+                                                      data: {
+                                                        jdCode: res.code,
+                                                        nickName: e.userInfo.nickName,
+                                                        gender: e.userInfo.gender,
+                                                        country: e.userInfo.country,
+                                                        province: e.userInfo.province,
+                                                        city: e.userInfo.city,
+                                                        avatarUrl: e.userInfo.avatarUrl,
+                                                      },
+                                                      header: {
+                                                        'content-type': 'application/json' // 默认值
+                                                      },
+                                                      success: function (res) {
+                                                        console.log(res.data)
+                                                        if (res.data.error == 'code-0000') {
+                                                          // 登录成功，用户信息成功保存
+                                                        }
+                                                      }
+                                                    })
+                                                  }
+                                                });
+                                              }
+                                            }
+                                          });
+                                        }
+                                      }
+                                    })
+                                  }
+                                });
+                              }
+                            }
+                          });
+                        }
+                      }
+                    })
+                  }
+                });
+              }
             }
-          })
+          });
+        } else {
+          wx.login({
+            success: function (res) {
+              if (res.code) {
+                wx.getUserInfo({
+                  lang: 'zh_CN',
+                  success(e) {
+                    _this.setData({
+                      userInfo: e.userInfo
+                    });
+                    //发起网络请求
+                    wx.request({
+                      url: 'http://127.0.0.1:8080/zlx/login',
+                      data: {
+                        jdCode: res.code,
+                        nickName: e.userInfo.nickName,
+                        gender: e.userInfo.gender,
+                        country: e.userInfo.country,
+                        province: e.userInfo.province,
+                        city: e.userInfo.city,
+                        avatarUrl: e.userInfo.avatarUrl,
+                      },
+                      header: {
+                        'content-type': 'application/json' // 默认值
+                      },
+                      success: function (res) {
+                        console.log(res.data)
+                        if (res.data.error == 'code-0000') {
+                          // 登录成功，用户信息成功保存
+                        } else {
+                          // 用户信息保存失败,重新获取
+                          wx.login({
+                            success: function (res) {
+                              if (res.code) {
+                                wx.getUserInfo({
+                                  lang: 'zh_CN',
+                                  success(e) {
+                                    console.log(e)
+                                    _this.setData({
+                                      userInfo: e.userInfo
+                                    });
+                                    //发起网络请求
+                                    wx.request({
+                                      url: 'http://127.0.0.1:8080/zlx/login',
+                                      data: {
+                                        jdCode: res.code,
+                                        nickName: e.userInfo.nickName,
+                                        gender: e.userInfo.gender,
+                                        country: e.userInfo.country,
+                                        province: e.userInfo.province,
+                                        city: e.userInfo.city,
+                                        avatarUrl: e.userInfo.avatarUrl,
+                                      },
+                                      header: {
+                                        'content-type': 'application/json' // 默认值
+                                      },
+                                      success: function (res) {
+                                        console.log(res.data)
+                                        if (res.data.error == 'code-0000') {
+                                          // 登录成功，用户信息成功保存
+                                        }
+                                      }
+                                    })
+                                  }
+                                });
+                              }
+                            }
+                          });
+                        }
+                      }
+                    })
+                  }
+                });
+              }
+            }
+          });
         }
-      }
-    });
-
-    // var _this = this;
-    // // 查看是否获取了用户信息
-    // wx.getSetting({
-    //   success(res) {
-    //     // 没有获取，就获取用户信息
-    //     if (!res.authSetting['scope.userInfo']) {
-    //       wx.authorize({
-    //         scope: 'scope.userInfo',
-    //         success(e) { // 用户同意直接获取
-    //           wx.getUserInfo({
-    //             success(e) {
-    //               _this.setData({
-    //                 userInfo: e.userInfo
-    //               });
-    //             }
-    //           })
-    //         },
-    //         fail(e) { // 用户拒绝直接跳转开启授权页面，引导用户开启
-    //           wx.openSetting({
-    //             success(e) {
-    //               // 用户同意授权
-    //               if (e.authSetting['scope.userInfo'] == true) {
-    //                 wx.getUserInfo({
-    //                   success(e) {
-    //                     _this.setData({
-    //                       userInfo: e.userInfo
-    //                     });
-    //                   }
-    //                 })
-    //               }
-    //             },
-    //           })
-    //         }
-    //       })
-    //     } else {
-    //       wx.getUserInfo({
-    //         success(e) {
-    //           _this.setData({
-    //             userInfo: e.userInfo
-    //           });
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+      },
+    })
   },
 
   // 获取用户信息
   getUserInfo: function () {
     var _this = this;
-    wx.getSetting({
-      success(res) {
-        if (!res.authSetting['scope.userInfo']) {
-          wx.authorize({
-            scope: 'scope.userInfo',
-            success(e) {
-              wx.getUserInfo({
-                success(e) {
-                  _this.setData({
-                    userInfo: e.userInfo
-                  });
-                }
-              })
-            },
-          })
-        } else {
-          wx.getUserInfo({
-            success(e) {
-              _this.setData({
-                userInfo: e.userInfo
-              });
+    wx.openSetting({
+      success(e) {
+        // 用户同意授权
+        if (e.authSetting['scope.userInfo'] == true) {
+          wx.login({
+            success: function (res) {
+              if (res.code) {
+                wx.getUserInfo({
+                  lang: 'zh_CN',
+                  success(e) {
+                    console.log(e)
+                    _this.setData({
+                      userInfo: e.userInfo
+                    });
+                    //发起网络请求
+                    wx.request({
+                      url: 'http://127.0.0.1:8080/zlx/login',
+                      data: {
+                        jdCode: res.code,
+                        nickName: e.userInfo.nickName,
+                        gender: e.userInfo.gender,
+                        country: e.userInfo.country,
+                        province: e.userInfo.province,
+                        city: e.userInfo.city,
+                        avatarUrl: e.userInfo.avatarUrl,
+                      },
+                      header: {
+                        'content-type': 'application/json' // 默认值
+                      },
+                      success: function (res) {
+                        console.log(res.data)
+                        if (res.data.error == 'code-0000') {
+                          // 登录成功，用户信息成功保存
+                        } else {
+                          // 用户信息保存失败
+                        }
+                      }
+                    })
+                  }
+                });
+              }
             }
-          })
-          if (_this.data.userInfo.nickName == null) {
-            wx.openSetting({
-              success(e) {
-                // 用户同意授权
-                if (e.authSetting['scope.userInfo'] == true) {
-                  wx.getUserInfo({
-                    success(e) {
-                      _this.setData({
-                        userInfo: e.userInfo
-                      });
-                    }
-                  })
-                }
-              },
-            })
-          }
+          });
         }
       }
     })
