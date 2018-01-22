05d3dedb2e13d681a948a998b49cfbbfe7626ee3
lllZLXlll <zlx_ooo@126.com>
2018-01-22 15:44:46 +0800

    首页
    banner查询

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,82 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+const serverUrl = app.globalData.serverUrl;
 
 Page({
   data: {
-    imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
-    ],
+    bannerList: [],
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 1000,
     circular: true,
   },
-  toPage: function(object) {
-    console.log(object)
-    wx.navigateTo({
-      url: '../productInfo/productInfo'
 
-      // url: '../../components/productSelect/productSelect'
+  onShow: function (e) {
+    this.getData();
+  },
+
+  // 展示加载框
+  showToast: function () {
+    wx.showLoading({
+      title: '加载中...',
+      // mask: true,
     })
+  },
+
+  // 隐藏加载框
+  hideoast: function () {
+    wx.hideLoading();
+  },
+
+  getData: function () {
+    var _this = this;
+
+    _this.showToast();
+    // 发起网络请求
+    wx.request({
+      url: serverUrl + 'queryHomeData',
+      success: function (res) {
+        if (res.data.error == 'code-0000') {
+          _this.hideoast();
+
+          _this.setData({
+            bannerList: res.data.bannerList
+          });
+        }
+      },
+      complete: function (e) {
+        if (e.errMsg != app.globalData.requestOk) {
+          _this.hideoast();
+          if (e.errMsg == app.globalData.requestTimeout) {
+            wx.showToast({
+              title: '网络请求超时',
+              image: '../../images/user/icon_error.png'
+            });
+          } else if (e.errMsg == app.globalData.requestFail) {
+            wx.showToast({
+              title: '网络请求失败',
+              image: '../../images/user/icon_error.png'
+            });
+          } else {
+            wx.showToast({
+              title: '请求失败',
+              image: '../../images/user/icon_error.png'
+            });
+          }
+        }
+      }
+    });
+  },
+
+
+
+  toProductInfo: function (e) {
+    if (e.currentTarget.dataset.id) {
+      wx.navigateTo({
+        url: '../productInfo/productInfo?id=' + e.currentTarget.dataset.id
+      })
+    }
   },
 })
